3c4f57f7c5e584b8b2438efde005f412aa46cbe7
Jakub Zajac <Jakub.Zajac@ibm.com>
2026-03-22 12:31:36 +0100

    update pomiar

--- src/pomiar/__init__.py
+++ src/pomiar/__init__.py
@@ -1,3 +1,3 @@
-from .mapa_nadajniki import df_transmitters, plot_map, plot_transmitters_on_map
+from .mapa_nadajniki import df_transmitters, plot_map, plot_transmitters_on_map, bounds
 from .data_loader import id_mapping, calc_boxplot_data, read_pomiar_data
 __all__ = ["df_transmitters", "plot_map","plot_transmitters_on_map", "id_mapping", "calc_boxplot_data","read_pomiar_data"]
