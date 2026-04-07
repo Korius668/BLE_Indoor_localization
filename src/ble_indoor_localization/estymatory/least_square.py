af0a8bf7c7c70ae55e53c9d1baa6e48b840587c1
Korius668 <korius668@gmail.com>
2026-04-07 19:12:22 +0200

    update least square

--- src/ble_indoor_localization/estymatory/least_square.py
+++ src/ble_indoor_localization/estymatory/least_square.py
@@ -28,27 +28,39 @@ def objective_function(position, beacons, distances_from_rssi, weights=None, dis
         residuals = geometrical_distances - distances_from_rssi
     else:        
         residuals = weights*(geometrical_distances - distances_from_rssi)
-    return residuals/(distances_from_rssi**distance_factor) if distance_factor != 1 else residuals/distances_from_rssi
-
+    if distance_factor == 0:
+        return residuals
+    elif distance_factor == 1:
+        return residuals/distances_from_rssi
+    else:
+        return residuals/(distances_from_rssi**distance_factor)
 
 def least_square_estimation(df,df_transmitters, bounds=None, func=objective_function, distance_factor=1.0):
 
     distances_from_rssi = []
     beacons_coords = []
+    weights = []
+    time_start = df['data'].min()
+    full_duration = df['data'].max() - time_start
     
     for index, row in df.iterrows():
         i = row['id nadajnika']
         rssi_value = row['znormalizowana moc sygnalu']
         distances_from_rssi.append(calculate_distance_from_rssi(rssi_value))
         beacons_coords.append((df_transmitters.loc[df_transmitters['Id'] == int(i), 'x'].values[0], df_transmitters.loc[df_transmitters['Id'] == int(i), 'y'].values[0]))
+        time = time_start-row['data']
+        if full_duration.total_seconds() > 0:
+            weights.append(time.total_seconds()/full_duration.total_seconds())
+    
     beacons_coords= np.array(beacons_coords)
     distances_from_rssi = np.array(distances_from_rssi)
+    weights = np.array(weights)
     initial_guess = beacons_coords[np.argmin(distances_from_rssi)]
     if bounds is not None:
         position = least_squares(
             func,
             initial_guess,
-            args=(beacons_coords, distances_from_rssi, None, distance_factor),
+            args=(beacons_coords, distances_from_rssi, weights, distance_factor),
             bounds=bounds,
             loss='soft_l1', 
             f_scale=1.0
@@ -57,7 +69,7 @@ def least_square_estimation(df,df_transmitters, bounds=None, func=objective_func
             position = least_squares(
             func,
             initial_guess,
-            args=(beacons_coords, distances_from_rssi, None, distance_factor),
+            args=(beacons_coords, distances_from_rssi, weights, distance_factor),
             loss='soft_l1', 
             f_scale=1.0
         )
