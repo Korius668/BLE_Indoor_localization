d7c40430d04e12197b92fda5146ade02985a1a56
Korius668 <korius668@gmail.com>
2026-05-16 19:02:05 +0200

    geaerg

--- src/pomiar1/dystans.py
+++ src/pomiar1/dystans.py
@@ -1,26 +1,30 @@
 from typing import Any, Callable
-
+from tqdm import tqdm
 
 import matplotlib.pyplot as plt
 
 from ble_indoor_localization import (
     calculate_distance_from_rssi, 
-    plot_distance_from_signal
+    plot_distance_from_signal,    
 )
+from ble_indoor_localization.plotting import plot_example_distance_from_signal
 from pomiar import df_transmitters, plot_map
 
 from .boxplot import dfs
 from .regresja_liniowa import model
 
 
+
 if __name__ == "__main__":
-    for measurement_name, df_measurement in dfs.items():
+    for measurement_name, df_measurement in tqdm(dfs.items()):
+        if measurement_name > 1:
+            break
         fig, ax = plt.subplots(figsize=(8, 8))
         ax= plot_map()
         func = lambda rssi: calculate_distance_from_rssi(rssi, model)
-        plot_distance_from_signal(measurement_name, df_measurement, df_transmitters, calculate_distance_func=func,  ax=ax)
-        ax.set_ylim(-10, 42)
-        ax.set_xlim(-20, 20)
+        plot_example_distance_from_signal(measurement_name, df_measurement, df_transmitters, calculate_distance_func=func, ax=ax,)
+        ax.set_ylim(-1, 20)
+        ax.set_xlim(-7, 10)
         ax.set_xlabel('Oś X (m)')
         ax.set_ylabel('Oś Y (m)')
     plt.show()
